b95d227da9b8d160936553453ed17ced637042f6
Hidayat arghandabi <hidayatarg@gmail.com>
2018-11-28 21:28:53 +0100

    Updated 4th question

--- Question4.py
+++ Question4.py
@@ -18,16 +18,18 @@ print(randomList[0])
 
 Number = []
 
-for i in range(4):
-    x = int(input("enter no.{0}: ".format(i + 1)))
-    Number.insert(i, x)
-    i += 1
+win=False
 
 print(Number)
 j = 0
 z = 0
 digitLocation = 0
 while j<10:
+    for i in range(3):
+        x = int(input("enter no.{0}: ".format(i + 1)))
+        Number.insert(i, x)
+        i += 1
+
     for i in range(3):
         if Number[i] == randomList[z]:
             if digitLocation == z:
@@ -62,20 +64,21 @@ while j<10:
             if(Number[i]==randomList[i]):
                 if(y==4):
                     print("You win the game")
+                    win=True
+                    break
                 y += 1
-                
+    if(win=True):
+        break            
     j += 1
-    print("You have " + j + " chances to play")
+    print("You have {0} chances to play".format(j))
 
 
-#Allow user to guess the number
+# Allow user to guess the number
 
-
-#If the user guess match the target number and the location
+# If the user guess match the target number and the location
 
 # For each correct print a +  if match but different location -
 
 # if not match and (not  location and value it is blank)
-
 
 # 10 chances if guess right winning message or print the tries
