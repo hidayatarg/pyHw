b83c9ee8e38cc11f5c62ce3030eec96d7f9ee2d4
Hidayat arghandabi <hidayatarg@gmail.com>
2018-12-30 17:31:19 +0300

    Updated

--- Homework-3.py
+++ Homework-3.py
@@ -1,15 +1,10 @@
 import math
-# implement a dot product function for 
-# matrix multiplication
 
+# Input will be your student number which consists of 11 integers and this 
+# information will be held in a list
+studentNumber = [1, 1, 0, 7, 0, 0, 0, 1, 0, 2, 6]
 
-# a function to transform given values of a list
-
-# Input will be your student number which consists 
-# of 11 integers and this information will be held 
-# in a list
-
-# another matrix
+# another matrix 
 w = [[0.03370361335647359],
      [0.07661115022886011],
      [0.04666099316268933],
@@ -22,32 +17,43 @@ w = [[0.03370361335647359],
      [0.038102552367237044],
      [0.029275344822365088]]
 
-studentNumber = [1, 1, 0, 7, 0, 0, 0, 1, 0, 2, 6]
+# Lists to store the Raw outputs and shift outputs
+c = []
+d = []
 
-# X = [1, 2, 3, 4, 5, 6]
-# print(studentNumber)
-# print(w)
-
-
-
-def list_shift(my_list, shift):
+# List shifting function
+def shiftList(my_list, shift):
     assert shift < len(my_list)
     return my_list[shift:] + my_list[:shift]
-c=[]
-counter1 = 1
+
+#implement a dot product function for matrix multiplication
 def dotMultiplication(v, w):
     a = sum([x*y for (x, *x2), y in zip(w, v)])
     b = round(a, 6)
-    print(counter1,":",b)
+    # print(counter1,":",b)
+    d.append(b)
     c.append(1/(1+math.exp(-b)))
-    
 
+# Printing the list    
+print("input=[{}]".format(studentNumber))
+# Sending the intial list to dot product
 dotMultiplication(studentNumber, w)
+
+# Output after each shift by 1
 for i in range(len(studentNumber)-1):
-    studentNumber = list_shift(studentNumber, 1)
+    studentNumber = shiftList(studentNumber, 1)
+    print("input=[{}]".format(studentNumber))
     dotMultiplication(studentNumber, w)
 
-print("outfunction")
+# Print Raw Output for shift
+# print("Raw Output for shift")
+counter = 1
+for cs in d:
+    print("Raw Output for shift", counter, ":", round(cs, 6))
+    counter += 1
+
+# Output for shift
+# print("Output for shift")
 counter=1
 for cs in c:
     print("Output for shift",counter,":", round(cs, 6))
